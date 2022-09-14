97c5a3d315b2ebbf0de0e86db690742d462337b3
Dmitriy B <kupperfeet@gmail.com>
2022-09-14 19:22:45 +0300

    task 2 done - count m a n, added gitigore

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void NumberCount(int m, int n)
+{
+    if (m <= n)
+    { 
+        Console.Write($"{m} ");
+        NumberCount(m+1, n);
+    }
+    else Console.Write(" ");
+}
+
+Console.Write("Введите начало отсчета: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+NumberCount(m, n);
