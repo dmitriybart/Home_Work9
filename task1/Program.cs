69a19e3d05ff05fbd322a5bddce5e2734ff40b5f
Dmitriy B <kupperfeet@gmail.com>
2022-09-14 19:18:26 +0300

    task 1 done - count n a 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать натуральные числа от N до 1, N задано
+
+void NumberCount( int n)
+{
+    if (1 <= n)
+    { 
+        Console.Write($"{n} ");
+        NumberCount(n-1);
+    }
+    else Console.Write(" ");
+}
+
+
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+NumberCount(n);
