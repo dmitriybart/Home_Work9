842baf9e9ed7d70c5a5f6e6d4fc6ea90a032ec5c
Dmitriy B <kupperfeet@gmail.com>
2022-09-14 20:15:54 +0300

    task 3 done sum m a n

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumNumber(int m, int n)
+{
+    if(m == n+1) return 0;
+    else
+    { 
+        int sum = m + SumNumber(m+1, n);
+        return sum;
+    }
+}
+
+Console.Write("Введите 1-е число: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите 2-е число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write(SumNumber(m,n));
+
