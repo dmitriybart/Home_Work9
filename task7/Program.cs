5237d3b3a1f5d3026241957a46a5a9e3ede90c22
Dmitriy B <kupperfeet@gmail.com>
2022-09-18 18:57:04 +0300

    task 7 done fibo recursiv

---
+++ task7/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу показывающие первые N чисел, 
+// для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+
+int Fibonacci(int p, int t, int position)
+{
+    if (position == 1) return p;
+    if (position == 2) return t;
+    return Fibonacci(p, t, position - 2) + Fibonacci(p, t, position - 1);
+}
+Console.Write("Введите 1-е число: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите 2-е число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+void PrintNumbers(int count, int newPosition = 0)
+{
+    newPosition++;
+    if(count >= newPosition)
+    {
+        Console.Write($"{Fibonacci(m, n, newPosition)} ");
+        PrintNumbers(count, newPosition);
+    }
+    else return; 
+}
+
+Console.Write("Введите количество чисел в ряду: ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+PrintNumbers(k);
