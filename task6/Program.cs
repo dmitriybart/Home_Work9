b8d717e28eed6ba8b8ae7e6c1b60d303e1df1d86
Dmitriy B <kupperfeet@gmail.com>
2022-09-18 14:13:46 +0300

    task 6 done power number

---
+++ task6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int PowNumber(int userNumber, int power)
+{
+    if(power == 0) return 1;
+    if(power == 1) return userNumber;
+    else
+    { 
+        int result = userNumber * PowNumber(userNumber, power-1);
+        return result;
+    }
+}
+
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите степень числа: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write(PowNumber(m,n));
